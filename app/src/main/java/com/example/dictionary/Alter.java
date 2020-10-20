bafb7ce6096d0e7377a3ec251b21bd0da6a5b82f
succeessful <18212325202@163.com>
2020-10-20 09:39:02 +0800

    10.20test

---
+++ app/src/main/java/com/example/dictionary/Alter.java
@@ -0,0 +1,13 @@
+package com.example.dictionary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Alter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alter);
+    }
+}
